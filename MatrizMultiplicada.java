8793d31cf59d04abe8eca0e84678d57249f9bcd9
Sandra-aguilar <62567980+Sandra-aguilar@users.noreply.github.com>
2020-07-03 03:51:46 -0500

    Add files via upload

---
+++ MatrizMultiplicada.java
@@ -0,0 +1,14 @@
+package matriz.multiplicada;
+
+
+public class MatrizMultiplicada {
+
+  
+    public static void main(String[] args) {
+Matriz e1=new Matriz();
+e1.matriz();
+e1.imprimir();
+ 
+    }   
+}
+       
